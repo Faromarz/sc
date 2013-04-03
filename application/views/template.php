abba6fcc9557b84c5fea591aeb3d44ed2ab77891
Tom Lankhorst <info@tomlankhorst.nl>
2013-04-03 12:43:35 +0300

    Added Google Analytics

--- application/views/template.php
+++ application/views/template.php
@@ -79,6 +79,19 @@
                 <a href="#" class="btn" id="agenda-signup-cancel">Annuleren</a>
             </div>
         </div>
+        <script type="text/javascript">
         
+          var _gaq = _gaq || [];
+          _gaq.push(['_setAccount', 'UA-27893460-2']);
+          _gaq.push(['_setDomainName', 'soul-coaching.nl']);
+          _gaq.push(['_trackPageview']);
+        
+          (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+          })();
+        
+        </script>
     </body>
 </html>
